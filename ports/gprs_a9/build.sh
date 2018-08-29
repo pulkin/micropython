6f7b261afd1475b931f21900287c68b06b1e1c6a
Neucrack <CZD666666@gmail.com>
2018-08-29 18:07:40 +0800

    compile ok

---
+++ ports/gprs_a9/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd GPRS_C_SDK/platform/tools/genlib
+./genlib.sh
---
+++ ports/gprs_a9/unistd.h
@@ -0,0 +1,9 @@
+#ifndef MICROPY_INCLUDED_GPRS_A9_UNISTD_H
+#define MICROPY_INCLUDED_GPRS_A9_UNISTD_H
+
+#include "cs_types.h"
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+
+#endif // MICROPY_INCLUDED_GPRS_A9_UNISTD_H
