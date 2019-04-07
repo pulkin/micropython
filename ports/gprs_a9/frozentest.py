8bb9fe99d2e1373444c66637fa33bcb72e752179
Artem <gpulkin@gmail.com>
2019-04-07 22:27:27 +0200

    Add options to test

--- ports/gprs_a9/frozentest.py
+++ ports/gprs_a9/frozentest.py
@@ -1,3 +1,11 @@
+# Tests sending SMS
+test_send_sms = None
+# test_send_sms = "8800"
+
+# Tests filesystem manipulations
+test_fs = False
+# test_fs = True
+
 print("================")
 print("std")
 print("================")
@@ -8,6 +16,109 @@ print(b'bytes 1234\x01')
 print(123456789)
 for i in range(4):
     print(i)
+
+if test_fs:
+    print("================")
+    print("filesystem")
+    print("================")
+
+    import os
+
+    os.chdir("/")
+    cwd = os.getcwd()
+    print("CWD:", cwd)
+    assert cwd == "/"
+    nfiles = len(os.listdir())
+    print("Files:", nfiles)
+
+    test_file = "__test_file__"
+    test_contents = "abcDEF"
+    f = open(test_file, 'w')
+    f.write(test_contents)
+    f.close()
+    f = open(test_file, 'r')
+    data = f.read()
+    f.close()
+    print("Contents:", data)
+    assert data == test_contents
+
+    f = open(test_file, 'w')
+    f.write(test_contents[:-1])
+    f.close()
+    f = open(test_file, 'r')
+    data = f.read()
+    f.close()
+    print("Contents:", data)
+    assert data == test_contents[:-1]
+
+    os.remove(test_file)
+    _nfiles = len(os.listdir())
+    print("Files:", _nfiles)
+    assert nfiles == _nfiles
+
+    test_dir1 = "__temp_test_dir__"
+    os.mkdir(test_dir1)
+    os.chdir(test_dir1)
+    cwd = os.getcwd()
+    print("CWD:", cwd)
+    assert cwd == "/"+test_dir1
+
+    test_dir2 = "__temp_test_dir2__"
+    os.mkdir(test_dir2)
+    os.chdir(test_dir2)
+    cwd = os.getcwd()
+    print("CWD:", cwd)
+    assert cwd == "/"+test_dir1+"/"+test_dir2
+
+    ls = os.listdir()
+    print("Files:", ls)
+    assert len(ls) == 0
+
+    test_file = "__test_file__"
+    test_contents = "abcDEF"
+    f = open(test_file, 'w')
+    f.write(test_contents)
+    f.close()
+    ls = os.listdir()
+    print("Files:", ls)
+    assert len(ls) == 1
+    assert ls[0] == test_file
+
+    f = open(test_file, 'r')
+    data = f.read()
+    f.close()
+    print("Contents:", data)
+    assert data == test_contents
+
+    os.chdir("/")
+
+    f = open("/" + test_dir1 + "/" + test_dir2 + "/" + test_file, 'r')
+    data = f.read()
+    f.close()
+    print("Contents:", data)
+    assert data == test_contents
+
+    ls = os.listdir("/" + test_dir1)
+    print("Files:", ls)
+    assert len(ls) == 1
+    assert ls[0] == test_dir2
+
+    ls = os.listdir("/" + test_dir1 + "/" + test_dir2)
+    print("Files:", ls)
+    assert len(ls) == 1
+    assert ls[0] == test_file
+
+    os.remove("/" + test_dir1 + "/" + test_dir2 + "/" + test_file)
+    ls = os.listdir("/" + test_dir1 + "/" + test_dir2)
+    print("Files:", ls)
+    assert len(ls) == 0
+
+    os.rmdir("/" + test_dir1 + "/" + test_dir2)
+    ls = os.listdir("/" + test_dir1)
+    print("Files:", ls)
+    assert len(ls) == 0
+
+    os.rmdir("/" + test_dir1)
 
 print("================")
 print("GPS")
@@ -104,10 +215,9 @@ if sim_present:
     for i in sms_list:
         assert isinstance(i, cel.SMS)
 
-    # A free LEBARA number to send SMS to
-    sms = cel.SMS("8800", "hello")
-    sms.send()
-    cel.poll_network_exception()
+    if test_send_sms is not None:
+        sms = cel.SMS(test_send_sms, "hello")
+        sms.send()
 
     print("----------------")
     print("GPRS")
