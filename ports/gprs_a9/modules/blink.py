d84ce2643897a28445e2fe96ebe08dd0033a45b4
Artem <gpulkin@gmail.com>
2020-01-25 17:49:32 +0100

    Fix formatting for built-in blink

--- ports/gprs_a9/modules/blink.py
+++ ports/gprs_a9/modules/blink.py
@@ -1,14 +1,12 @@
-
 import machine
 import time
 
 def blink(interval):
     print("blink")
-    led = machine.Pin(27,machine.Pin.OUT,0)
+    led = machine.Pin(27, machine.Pin.OUT, 0)
     value = 1
-    while(1):
+    while True:
         led.value(value)
         time.sleep(interval)
         value = 0 if (value==1) else 1
-
 
