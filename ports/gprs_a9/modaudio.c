9c61c5a7c4b3ed4e31c1b9c8742e6348cea0d2b4
Ubaldo Porcheddu <ubaldo@eja.it>
2020-09-04 13:10:19 +0100

    removed sleep loop in record and play functions and renamed both to play_start and record_start

--- ports/gprs_a9/modaudio.c
+++ ports/gprs_a9/modaudio.c
@@ -32,106 +32,79 @@
 #include "api_fs.h"
 #include "py/runtime.h"
 
-uint8_t audioTimeout = 5;
 uint8_t audioVolume = 5;
-bool audioPlayEnd = true;
+int audioRecordFd = -1;
 
 
-void audioRecordCallback(AUDIO_Error_t result)
-{
-    if (result != AUDIO_ERROR_NO) {
-        Trace(1,"audio record error: %d", result);
-    }
-}
-
-
-STATIC mp_obj_t modaudio_record(size_t n_args, const mp_obj_t *args) {
+STATIC mp_obj_t modaudio_record_start(size_t n_args, const mp_obj_t *args) {
     // ========================================
     // Audio Record.
     // Args:
     //     filename (str): output file name with full path
-    //     timeout (int): timeout in seconds (default 5)
     //	   channel (int): audio channel (default 2, AUDIO_MODE_LOUDSPEAKER)
     //	   type (int): audio type (default 3, AUDIO_TYPE_AMR)
     //	   mode (int): audio mode (default 7, AUDIO_RECORD_MODE_AMR122)
     // ========================================
     
-    int timeout = audioTimeout;
+    const char* filename;    
     int channel = AUDIO_MODE_LOUDSPEAKER;
     int type = AUDIO_TYPE_AMR;
     int mode = AUDIO_RECORD_MODE_AMR122;
-    const char* filename;
     
     if (n_args > 0) {
-     filename = mp_obj_str_get_str(args[0]);
+        filename = mp_obj_str_get_str(args[0]);
     } else {
         mp_raise_ValueError("filename is mandatory");
         return mp_const_none;
     }
     if (n_args >= 2) {
-        timeout = mp_obj_get_int(args[1]); 
+        channel = mp_obj_get_int(args[1]);
     }
     if (n_args >= 3) {
-        channel = mp_obj_get_int(args[2]);
-    }
-    if (n_args >= 3) {
-        type = mp_obj_get_int(args[3]);
+        type = mp_obj_get_int(args[2]);
     }
     if (n_args >= 4) {
-        mode = mp_obj_get_int(args[4]);
+        mode = mp_obj_get_int(args[3]);
     }
-    Trace(1,"record start, filename: %s, timeout: %d, channel: %d, type: %d, mode: %d", filename, timeout, channel, type, mode);
-    int audioRecordFd = API_FS_Open(filename,FS_O_WRONLY|FS_O_CREAT|FS_O_TRUNC,0);
-    if(audioRecordFd < 0) {
+    Trace(1,"audio record start, filename: %s, channel: %d, type: %d, mode: %d", filename, channel, type, mode);
+    audioRecordFd = API_FS_Open(filename,FS_O_WRONLY|FS_O_CREAT|FS_O_TRUNC,0);
+    if (audioRecordFd < 0) {
         Trace(1,"audio open file error");
-        mp_raise_ValueError("file error");
+        mp_raise_ValueError("file open error");
         return mp_const_none;
     }
     AUDIO_SetMode(channel);
-    AUDIO_Error_t result =  AUDIO_RecordStart(type, mode, audioRecordFd, audioRecordCallback, NULL);
+    AUDIO_Error_t result =  AUDIO_RecordStart(type, mode, audioRecordFd, NULL, NULL);
     if (result != AUDIO_ERROR_NO) {
-        Trace(1,"record start fail: %d",result);
+        Trace(1,"audio record start fail: %d",result);
         API_FS_Close(audioRecordFd);
         audioRecordFd = -1;
-        mp_raise_ValueError("cannot start recording");
+        mp_raise_ValueError("cannot record audio file");
         return mp_const_none;
     }
-    Trace(1,"record start");
-    while(--timeout)
-    {
-        Trace(1,"recording");
-        OS_Sleep(1000);
-    }
-    Trace(1,"record stop");
-    AUDIO_RecordStop();
-    API_FS_Close(audioRecordFd);
-    audioRecordFd = -1;
-    Trace(1,"record end");
+    Trace(1,"recording");
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modaudio_record_obj, 1, 2, modaudio_record);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modaudio_record_start_obj, 1, 2, modaudio_record_start);
 
 
 STATIC mp_obj_t modaudio_record_stop(void) {
     //Stop recording audio
     AUDIO_RecordStop();
+    API_FS_Close(audioRecordFd);
+    audioRecordFd = -1;
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_record_stop_obj, modaudio_record_stop);
 
 
-void audioPlayCallback(AUDIO_Error_t result)
-{
-    Trace(1,"play callback result: %d",result);
-    if (AUDIO_ERROR_END_OF_FILE == result) {
-        Trace(1,"play music file end");
-    }
-    audioPlayEnd = true;
+void audioPlayCallback(AUDIO_Error_t result) {
+    Trace(1,"audio play callback result: %d",result);
     AUDIO_Stop();
 }
 
 
-STATIC mp_obj_t modaudio_play(size_t n_args, const mp_obj_t *args) {
+STATIC mp_obj_t modaudio_play_start(size_t n_args, const mp_obj_t *args) {
     // ========================================
     // Audio Play.
     // Args:
@@ -140,10 +113,12 @@ STATIC mp_obj_t modaudio_play(size_t n_args, const mp_obj_t *args) {
     //	   channel (int): audio channel (default 2, AUDIO_MODE_LOUDSPEAKER)
     //	   type (int): audio type (default 3, AUDIO_TYPE_AMR)
     // ========================================
+
+    const char* filename;    
     int volume = audioVolume;
     int channel = AUDIO_MODE_LOUDSPEAKER;
     int type = AUDIO_TYPE_AMR;
-    const char* filename;
+
     if (n_args > 0) {
      filename = mp_obj_str_get_str(args[0]);
     } else {
@@ -156,55 +131,43 @@ STATIC mp_obj_t modaudio_play(size_t n_args, const mp_obj_t *args) {
     if (n_args >= 3) {
         channel = mp_obj_get_int(args[2]);
     }
-    if (n_args >= 3) {
+    if (n_args >= 4) {
         type = mp_obj_get_int(args[3]);
     }
     Trace(1,"play start, filename: %s, volume: %d, channel: %d, type: %d", filename, volume, channel, type);
-    
-    AUDIO_Error_t ret = AUDIO_Play(filename, type, audioPlayCallback);
-    if( ret != AUDIO_ERROR_NO)
-    {
-        Trace(1,"play music fail: %d",ret);
-        mp_raise_ValueError("cannot play file");        
+    AUDIO_Error_t result = AUDIO_Play(filename, type, audioPlayCallback);
+    if (result != AUDIO_ERROR_NO) {
+        Trace(1,"play fail: %d", result);
+        mp_raise_ValueError("cannot play audio file");        
         return mp_const_none;        
-    }
-    else
-    {
-        audioPlayEnd = false;
-        Trace(1,"play music success");
+    } else {
+        Trace(1,"playing");
     }
     OS_Sleep(200);
     AUDIO_SetMode(channel);
     AUDIO_SpeakerOpen();
     AUDIO_SpeakerSetVolume(volume);
-
-    uint8_t i = AUDIO_EQ_NORMAL;
-    while(!audioPlayEnd)
-    {
-        OS_Sleep(1000);
-    }
-
-    Trace(1,"play end");
+    Trace(1,"playing");
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modaudio_play_obj, 1, 2, modaudio_play);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(modaudio_play_start_obj, 1, 2, modaudio_play_start);
 
-/*
-STATIC mp_obj_t modaudio_pause(void) {
+
+STATIC mp_obj_t modaudio_play_pause(void) {
     //Pause audio play
     AUDIO_Pause();
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_pause_obj, modaudio_pause);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_play_pause_obj, modaudio_play_pause);
 
 
-STATIC mp_obj_t modaudio_resume(void) {
-   //Resume playing from pause
-   AUDIO_Resume();
+STATIC mp_obj_t modaudio_play_resume(void) {
+   //Resume playing after pause
+   //AUDIO_Resume(fileFd); //?AUDIO_Play doesn't have nor return a fileFd
    return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_resume_obj, modaudio_resume);
-*/
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_play_resume_obj, modaudio_play_resume);
+
 
 STATIC mp_obj_t modaudio_play_stop(void) {
     //Stop playing audio
@@ -217,10 +180,12 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(modaudio_play_stop_obj, modaudio_play_stop);
 STATIC const mp_map_elem_t mp_module_audio_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_audio) },
 
-    { MP_OBJ_NEW_QSTR(MP_QSTR_record), (mp_obj_t)&modaudio_record_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_record_start), (mp_obj_t)&modaudio_record_start_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_record_stop), (mp_obj_t)&modaudio_record_stop_obj },    
-    { MP_OBJ_NEW_QSTR(MP_QSTR_play), (mp_obj_t)&modaudio_play_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_play_start), (mp_obj_t)&modaudio_play_start_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_play_stop), (mp_obj_t)&modaudio_play_stop_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_play_pause), (mp_obj_t)&modaudio_play_pause_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_play_resume), (mp_obj_t)&modaudio_play_resume_obj },
 };
 STATIC MP_DEFINE_CONST_DICT(mp_module_audio_globals, mp_module_audio_globals_table);
 
